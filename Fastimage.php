a274f245a9c7bfff4b3243456a1a187c89e996b3
Chen.Zhidong <njutczd@gmail.com>
2015-10-26 21:40:28 +0800

    add webp support

--- Fastimage.php
+++ Fastimage.php
@@ -81,6 +81,8 @@ class FastImage
 					return $this->type = 'jpeg';
 				case chr(0x89).'P':
 					return $this->type = 'png';
+				case "RI":
+                    return $this->type = 'webp';
 				default:
 					return false;
 			}
@@ -103,7 +105,9 @@ class FastImage
 			case 'bmp':
 				return $this->parseSizeForBMP();
 			case 'jpeg':
-				return $this->parseSizeForJPEG();	    
+				return $this->parseSizeForJPEG();
+			case 'webp':
+                return $this->parseSizeForWEBP();
 		}
 		
 		return null;
@@ -194,6 +198,13 @@ class FastImage
 			}
 		}
 	}
+
+	private function parseSizeForWEBP() {
+		$chars = $this->getChars(30);
+		$result = unpack("C12/S9", $chars);
+
+		return array($result['8'], $result['9']);
+    }
 
 
 	private function getChars($n)
